301ccb9c5df73251d95603aa8f1fd0ee49a1e3c3
Hoeyby <alex.hoeyby@gmail.com>
2021-11-02 17:32:15 +0100

    Assignment

---
+++ Assignment_4/Assignment.py
@@ -0,0 +1,280 @@
+import copy
+import itertools
+
+
+class CSP:
+    def __init__(self):
+        # self.variables is a list of the variable names in the CSP
+        self.variables = []
+
+        # self.domains[i] is a list of legal values for variable i
+        self.domains = {}
+
+        # self.constraints[i][j] is a list of legal value pairs for
+        # the variable pair (i, j)
+        self.constraints = {}
+
+        self.callCount = 0
+        self.failCount = 0
+
+    def add_variable(self, name, domain):
+        """Add a new variable to the CSP. 'name' is the variable name
+        and 'domain' is a list of the legal values for the variable.
+        """
+        self.variables.append(name)
+        self.domains[name] = list(domain)
+        self.constraints[name] = {}
+
+    def get_all_possible_pairs(self, a, b):
+        """Get a list of all possible pairs (as tuples) of the values in
+        the lists 'a' and 'b', where the first component comes from list
+        'a' and the second component comes from list 'b'.
+        """
+        return itertools.product(a, b)
+
+    def get_all_arcs(self):
+        """Get a list of all arcs/constraints that have been defined in
+        the CSP. The arcs/constraints are represented as tuples (i, j),
+        indicating a constraint between variable 'i' and 'j'.
+        """
+        return [(i, j) for i in self.constraints for j in self.constraints[i]]
+
+    def get_all_neighboring_arcs(self, var):
+        """Get a list of all arcs/constraints going to/from variable
+        'var'. The arcs/constraints are represented as in get_all_arcs().
+        """
+        return [(i, var) for i in self.constraints[var]]
+
+    def add_constraint_one_way(self, i, j, filter_function):
+        """Add a new constraint between variables 'i' and 'j'. The legal
+        values are specified by supplying a function 'filter_function',
+        that returns True for legal value pairs and False for illegal
+        value pairs. This function only adds the constraint one way,
+        from i -> j. You must ensure that the function also gets called
+        to add the constraint the other way, j -> i, as all constraints
+        are supposed to be two-way connections!
+        """
+        if not j in self.constraints[i]:
+            # First, get a list of all possible pairs of values between variables i and j
+            self.constraints[i][j] = self.get_all_possible_pairs(self.domains[i], self.domains[j])
+
+        # Next, filter this list of value pairs through the function
+        # 'filter_function', so that only the legal value pairs remain
+        self.constraints[i][j] = list(filter(lambda value_pair: filter_function(*value_pair), self.constraints[i][j]))
+
+    def add_all_different_constraint(self, variables):
+        """Add an Alldiff constraint between all of the variables in the
+        list 'variables'.
+        """
+        for (i, j) in self.get_all_possible_pairs(variables, variables):
+            if i != j:
+                self.add_constraint_one_way(i, j, lambda x, y: x != y)
+
+    def backtracking_search(self):
+        """This functions starts the CSP solver and returns the found
+        solution.
+        """
+        # Make a so-called "deep copy" of the dictionary containing the
+        # domains of the CSP variables. The deep copy is required to
+        # ensure that any changes made to 'assignment' does not have any
+        # side effects elsewhere.
+        assignment = copy.deepcopy(self.domains)
+
+        # Run AC-3 on all constraints in the CSP, to weed out all of the
+        # values that are not arc-consistent to begin with
+        self.inference(assignment, self.get_all_arcs())
+
+        # Call backtrack with the partial assignment 'assignment'
+        return self.backtrack(assignment)
+
+    def backtrack(self, assignment):
+        """The function 'Backtrack' from the pseudocode in the
+        textbook.
+
+        The function is called recursively, with a partial assignment of
+        values 'assignment'. 'assignment' is a dictionary that contains
+        a list of all legal values for the variables that have *not* yet
+        been decided, and a list of only a single value for the
+        variables that *have* been decided.
+
+        When all of the variables in 'assignment' have lists of length
+        one, i.e. when all variables have been assigned a value, the
+        function should return 'assignment'. Otherwise, the search
+        should continue. When the function 'inference' is called to run
+        the AC-3 algorithm, the lists of legal values in 'assignment'
+        should get reduced as AC-3 discovers illegal values.
+
+        IMPORTANT: For every iteration of the for-loop in the
+        pseudocode, you need to make a deep copy of 'assignment' into a
+        new variable before changing it. Every iteration of the for-loop
+        should have a clean slate and not see any traces of the old
+        assignments and inferences that took place in previous
+        iterations of the loop.
+        """
+
+        self.callCount += 1
+
+        # Check for completion and prints solution.
+        if all(len(values) == 1 for values in assignment.values()):
+            print(f'\n\nSolution found!\nBacktrack was called {self.callCount} times it failed {self.failCount} times.')
+            print(f'The solved sudoku:')
+            print_sudoku_solution(assignment)
+            return assignment
+
+        var = self.select_unassigned_variable(assignment)
+
+        for value in assignment[var]:
+            AssignmentCopy = copy.deepcopy(assignment)
+            AssignmentCopy[var] = [value]
+
+            # Check for inconsistency
+            if self.inference(AssignmentCopy, self.get_all_neighboring_arcs(var)):
+                # No inconsistencies found
+                result = self.backtrack(AssignmentCopy)
+                if result: return result 
+        
+        self.failCount += 1
+        return False
+        
+
+    def select_unassigned_variable(self, assignment):
+        """The function 'Select-Unassigned-Variable' from the pseudocode
+        in the textbook. Should return the name of one of the variables
+        in 'assignment' that have not yet been decided, i.e. whose list
+        of legal values has a length greater than one.
+        """
+
+        # Checks the lenght of the values of a variable and returns the min
+        lengths = ((len(value), key) for key, value in assignment.items())
+        return min(tup for tup in lengths if tup[0] > 1)[1]
+
+
+    def inference(self, assignment, queue):
+        """The function 'AC-3' from the pseudocode in the textbook.
+        'assignment' is the current partial assignment, that contains
+        the lists of legal values for each undecided variable. 'queue'
+        is the initial queue of arcs that should be visited.
+        """
+
+        while len(queue):
+
+            i, j = queue.pop()
+
+            if self.revise(assignment, i, j): # True if revise change anything
+
+                if not assignment[i]: # Fail
+                    return False
+
+                elif len(assignment[i]) > 1:
+                    continue
+
+                for neighbour in self.get_all_neighboring_arcs(i):
+                    queue.append(neighbour)
+
+        return True # No more inconsistencies
+        
+
+
+
+    def revise(self, assignment, i, j):
+        """The function 'Revise' from the pseudocode in the textbook.
+        'assignment' is the current partial assignment, that contains
+        the lists of legal values for each undecided variable. 'i' and
+        'j' specifies the arc that should be visited. If a value is
+        found in variable i's domain that doesn't satisfy the constraint
+        between i and j, the value should be deleted from i's list of
+        legal values in 'assignment'.
+        """
+
+        revised = False
+
+        for x in assignment[i]:
+            if not any((x, y) in self.constraints[i][j] for y in assignment[j]): # Checks for inconsistencies
+                assignment[i].remove(x) # if found, remove inconsistency
+                revised = True
+
+        return revised
+
+
+
+def create_map_coloring_csp():
+    """Instantiate a CSP representing the map coloring problem from the
+    textbook. This can be useful for testing your CSP solver as you
+    develop your code.
+    """
+    csp = CSP()
+    states = ['WA', 'NT', 'Q', 'NSW', 'V', 'SA', 'T']
+    edges = {'SA': ['WA', 'NT', 'Q', 'NSW', 'V'], 'NT': ['WA', 'Q'], 'NSW': ['Q', 'V']}
+    colors = ['red', 'green', 'blue']
+    for state in states:
+        csp.add_variable(state, colors)
+    for state, other_states in edges.items():
+        for other_state in other_states:
+            csp.add_constraint_one_way(state, other_state, lambda i, j: i != j)
+            csp.add_constraint_one_way(other_state, state, lambda i, j: i != j)
+    return csp
+
+
+def create_sudoku_csp(filename):
+    """Instantiate a CSP representing the Sudoku board found in the text
+    file named 'filename' in the current directory.
+    """
+    csp = CSP()
+    board = list(map(lambda x: x.strip(), open(filename, 'r')))
+
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == '0':
+                csp.add_variable('%d-%d' % (row, col), list(map(str, range(1, 10))))
+            else:
+                csp.add_variable('%d-%d' % (row, col), [board[row][col]])
+
+    for row in range(9):
+        csp.add_all_different_constraint(['%d-%d' % (row, col) for col in range(9)])
+    for col in range(9):
+        csp.add_all_different_constraint(['%d-%d' % (row, col) for row in range(9)])
+    for box_row in range(3):
+        for box_col in range(3):
+            cells = []
+            for row in range(box_row * 3, (box_row + 1) * 3):
+                for col in range(box_col * 3, (box_col + 1) * 3):
+                    cells.append('%d-%d' % (row, col))
+            csp.add_all_different_constraint(cells)
+
+    return csp
+
+
+def print_sudoku_solution(solution):
+    """Convert the representation of a Sudoku solution as returned from
+    the method CSP.backtracking_search(), into a human readable
+    representation.
+    """
+    for row in range(9):
+        for col in range(9):
+            print(solution['%d-%d' % (row, col)][0], end=" "),
+            if col == 2 or col == 5:
+                print('|', end=" "),
+        print("")
+        if row == 2 or row == 5:
+            print('------+-------+------')
+
+
+def get_solutions():
+    csp_easy = create_sudoku_csp('easy.txt')
+    solved_easy = csp_easy.backtracking_search()
+
+    csp_medium = create_sudoku_csp('medium.txt')
+    solved_medium = csp_medium.backtracking_search()
+
+    csp_hard = create_sudoku_csp('hard.txt')
+    solved_hard = csp_hard.backtracking_search()
+
+    csp_very_hard = create_sudoku_csp('veryhard.txt')
+    solved_very_hard = csp_very_hard.backtracking_search()
+
+    csp_very_very_hard = create_sudoku_csp('reallyfknhard.txt')
+    solved_very_very_hard = csp_very_very_hard.backtracking_search()
+
+    
+if __name__ == "__main__":
+    get_solutions()
---
+++ Assignment_4/README.md
@@ -0,0 +1,90 @@
+# Assignment 4
+
+By: Alexander Høyby\
+October 25, 2021
+
+</br>
+
+It is clear by the number of backtrack calls and the number of failures that the boards had different difficulties. By solving them by hand I found that escpecially the harder boards required more complex thinking and it therefore makes sense that the algorithm failed more on those boards as it would have to try and fail for a lot of the 'non obvious' values.
+
+## Easy
+
+```
+Solution found!
+Backtrack was called 1 times it failed 0 times.
+The solved sudoku:    
+7 8 4 | 9 3 2 | 1 5 6 
+6 1 9 | 4 8 5 | 3 2 7 
+2 3 5 | 1 7 6 | 4 8 9 
+------+-------+------ 
+5 7 8 | 2 6 1 | 9 3 4 
+3 4 1 | 8 9 7 | 5 6 2 
+9 2 6 | 5 4 3 | 8 7 1
+------+-------+------
+4 5 3 | 7 2 9 | 6 1 8
+8 6 2 | 3 1 4 | 7 9 5
+1 9 7 | 6 5 8 | 2 4 3
+```
+
+</br>
+
+## Medium
+
+```
+Solution found!
+Backtrack was called 2 times it failed 0 times.
+The solved sudoku:
+8 7 5 | 9 3 6 | 1 4 2
+1 6 9 | 7 2 4 | 3 8 5
+2 4 3 | 8 5 1 | 6 7 9
+------+-------+------
+4 5 2 | 6 9 7 | 8 3 1
+9 8 6 | 4 1 3 | 2 5 7
+7 3 1 | 5 8 2 | 9 6 4
+------+-------+------
+5 1 7 | 3 6 9 | 4 2 8
+6 2 8 | 1 4 5 | 7 9 3
+3 9 4 | 2 7 8 | 5 1 6
+```
+
+</br>
+
+## Hard
+
+```
+Solution found!
+Backtrack was called 7 times it failed 2 times.
+The solved sudoku:
+1 5 2 | 3 4 6 | 8 9 7
+4 3 7 | 1 8 9 | 6 5 2
+6 8 9 | 5 7 2 | 3 1 4
+------+-------+------
+8 2 1 | 6 3 7 | 9 4 5
+5 4 3 | 8 9 1 | 7 2 6
+9 7 6 | 4 2 5 | 1 8 3
+------+-------+------
+7 9 8 | 2 5 3 | 4 6 1
+3 6 5 | 9 1 4 | 2 7 8
+2 1 4 | 7 6 8 | 5 3 9
+```
+
+</br>
+
+## Very hard
+
+```
+Solution found!
+Backtrack was called 56 times it failed 43 times.
+The solved sudoku:
+4 3 1 | 8 6 7 | 9 2 5
+6 5 2 | 4 9 1 | 3 8 7
+8 9 7 | 5 3 2 | 1 6 4
+------+-------+------
+3 8 4 | 9 7 6 | 5 1 2
+5 1 9 | 2 8 4 | 7 3 6
+2 7 6 | 3 1 5 | 8 4 9
+------+-------+------
+9 4 3 | 7 2 8 | 6 5 1
+7 6 5 | 1 4 3 | 2 9 8
+1 2 8 | 6 5 9 | 4 7 3
+```
